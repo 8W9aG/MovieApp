e67c0ffebdf8d597dda5d89922adcab8318f69b2
Will Sackfield <sackfield@spotify.com>
2017-04-09 07:00:59 -0400

    Initial cleanup of repository

--- MovieApp/AppDelegate.h
+++ MovieApp/AppDelegate.h
@@ -1,17 +1,7 @@
-//
-//  AppDelegate.h
-//  MovieApp
-//
-//  Created by Will Sackfield on 4/9/17.
-//  Copyright © 2017 sackfield. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
-
 @end
-
--- MovieApp/main.m
+++ MovieApp/main.m
@@ -1,15 +1,9 @@
-//
-//  main.m
-//  MovieApp
-//
-//  Created by Will Sackfield on 4/9/17.
-//  Copyright © 2017 sackfield. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
+
 #import "AppDelegate.h"
 
-int main(int argc, char * argv[]) {
+int main(int argc, char * argv[])
+{
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
